426fe9e1edd9a4006c43f53da62f28d5dee5af52
Nick <dreamer.lviv@gmail.com>
2024-06-10 01:04:50 +0300

    update styles

--- paper.py
+++ paper.py
@@ -20,16 +20,15 @@ white = 1
 def clear_display(epd):
     global image, draw
     epd.Clear(0xFF)
-    # image = Image.new('1', (epd.height, epd.width), 255)
-    # draw = ImageDraw.Draw(image)
-    # epd.display(epd.getbuffer(image))
+    image = Image.new('1', (epd.height, epd.width), 255)
+    draw = ImageDraw.Draw(image)
+    epd.display(epd.getbuffer(image))
 
 try:
     epd = epd2in13_V2.EPD()
     epd.init(epd.FULL_UPDATE)
     # epd.Clear(0xFF)
 
-    font15 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
     font16 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 16)
     font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
 
@@ -39,24 +38,23 @@ try:
     epd.displayPartBaseImage(epd.getbuffer(image))
     epd.init(epd.PART_UPDATE)
 
-    num = 0
     start_time=time.time()
     elapsed = time.time()-start_time
     while (time.time()-start_time) <= 1000:
         elapsed=time.time()-start_time
         bat = int(subprocess.check_output("upsc ups | grep -o 'battery.charge: [0-9]*' | grep -o '[0-9]*'", shell=True))
         print(bat)
-        # Draw battery level
-        draw.rectangle((150, 0, 200, 20), fill = white)
-        draw.text((150, 0), str(bat), font = font16, fill = black)
-        draw.text((170, 0), '%', font = font16, fill = black)
 
-        print(elapsed)
+        # Draw battery level text
+        draw.rectangle((145, 0, 165, 16), fill = white)
+        draw.text((145, 0), str(bat), font = font16, fill = black)
+        draw.text((165, 0), '%', font = font16, fill = black)
 
-        # progress = int(220-int(elapsed*100))
-        # draw.rectangle((120, 70, progress, 75), fill = black)
-        # draw.rectangle((progress, 70, 220, 75), fill = white)
-        # draw.rectangle([(progress,70),(220,75)],outline = black)
+        # Draw battery level rect
+        progress = int(bat*2.4) # batterry level in percentage
+        draw.rectangle((0, 25, progress, 30), fill = black)
+        draw.rectangle((progress, 25, 240, 30), fill = white)
+        draw.rectangle([(progress,25),(240,30)],outline = black)
 
         # Draw time
         draw.rectangle((120, 80, 220, 105), fill = white)
@@ -68,8 +66,8 @@ try:
 
 # Tests finished
     clear_display(epd)
-    draw.text((0, 0), 'Tests finished', font = font15, fill = 0)
-    draw.text((0, 15), 'Visit peppe8o.com for more tutorials', font = font15, fill = 0)
+    draw.text((0, 0), 'Tests finished', font = font16, fill = 0)
+    draw.text((0, 16), 'Visit peppe8o.com for more tutorials', font = font16, fill = 0)
     epd.display(epd.getbuffer(image))
     time.sleep(5)
 
